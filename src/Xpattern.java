158ce476f5042d81d03123b77d923a5d9c6fcbe0
Mastermindx33 <mastermindx33@gmail.com>
2021-10-22 12:11:51 +0530

    Update Xpattern.java

--- src/Xpattern.java
+++ src/Xpattern.java
@@ -1,7 +1,11 @@
+import java.util.*;
 class Xpattern {
     static void x_pattern(String str, int len) {
+        System.out.println("The X pattern of the word is : ");
+        System.out.println("");
         for (int i = 0; i < len; i++) {
             int j = len - 1 - i;
+            // System.out.println(j);
             for (int k = 0; k < len; k++) {
                 if (k == i || k == j) {
                     System.out.print(str.charAt(k));
@@ -15,7 +19,11 @@ class Xpattern {
 
     // Driver Code
     public static void main(String[] args) {
-        String word = "program";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter an odd String : [ eg: PROGRAM ] \n");
+        System.out.print(">> ");
+        String word = s.nextLine().toUpperCase();
+        System.out.println("");
         x_pattern(word, word.length());
     }
 }
